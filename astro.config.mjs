aee8087699b993bfdbe055b6e6cb1cc237c4d9b0
Leo Roese <leo@meltano.com>
2023-10-23 16:53:40 +0100

    better styling on hover cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,32 +6,39 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "LeoRoese Blog",
-    customCss: ["./src/tailwind.css"],
-    defaultLocale: "en",
-    logo: {
-      src: "./src/assets/logo.svg",
-      replacesTitle: true
-    },
-    social: {
-      github: "https://github.com/withastro/starlight"
-    },
-    sidebar: [{
-      label: "Outlines",
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: "How to use",
-        link: "/outlines/how-to-use/"
-      }]
-    }, {
-      label: "Notes",
-      autogenerate: {
-        directory: "notes"
-      }
-    }]
-  }), tailwind({
-    applyBaseStyles: false
-  }), react()]
+  integrations: [
+    starlight({
+      title: "LeoRoese Blog",
+      customCss: ["./src/tailwind.css"],
+      defaultLocale: "en",
+      logo: {
+        src: "./src/assets/logo.svg",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Outlines",
+          items: [
+            {
+              label: "How to use",
+              link: "/outlines/how-to-use/",
+            },
+          ],
+        },
+        {
+          label: "Notes",
+          autogenerate: {
+            directory: "notes",
+          },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
--- src/components/hover-card.tsx
+++ src/components/hover-card.tsx
@@ -2,45 +2,48 @@ import * as HoverCard from "@radix-ui/react-hover-card";
 
 type Props = {
   href: string;
+  hrefText: string;
   children: React.ReactNode;
   title: string;
   type: string;
 };
 
-const HoverCardDemo = ({ href, children, title, type }: Props) => {
+const HoverCardDemo = ({
+  href,
+  hrefText = href,
+  children,
+  title,
+  type,
+}: Props) => {
   return (
     <HoverCard.Root>
       <HoverCard.Trigger asChild>
-        <a
-          className="inline-block cursor-pointer outline-none"
-          href={href}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {href}
+        <a className="inline-block cursor-pointer outline-none" href={href}>
+          {hrefText}
         </a>
       </HoverCard.Trigger>
       <HoverCard.Portal>
         <HoverCard.Content
-          className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
+          className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[360px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all max-h-80 overflow-y-scroll"
           side="right"
           sideOffset={5}
         >
           <div className="flex flex-col gap-[7px]">
             <div className="flex flex-col gap-[15px]">
               <div>
-                <div className="text-mauve12 m-0 text-[15px] font-semibold leading-[1.5]">
+                <a
+                  href={href}
+                  className="text-mauve12 dark:text-black m-0 text-[15px] font-semibold leading-[1.5]"
+                >
                   {title}
-                </div>
-                <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">
+                </a>
+                <div className="text-mauve10 dark:text-slate-700 m-0 text-[15px] leading-[1.5]">
                   {type}
                 </div>
               </div>
               <div className="text-mauve12 m-0 text-[15px] leading-[1.5]">
-                Components, icons, colors, and templates for building
-                high-quality, accessible UI. Free and open-source.
+                {children}
               </div>
-              <div>{children}</div>
             </div>
           </div>
 
